300efb9b91182b27df43018cc1e92d0c8c8b09ad
Ali Aliyev <elvinaliyev724@gmail.com>
2024-03-17 22:39:53 +0400

    method

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //IsPrime(10);
+
+
+
+
+            //CalcAvg(70,50,60,40,70,86);
+          
+        }
+
+        //static void IsPrime(int value)
+        //{
+
+        //    if (value%2==0)
+        //    {
+        //        Console.WriteLine("false");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(true);
+        //    }
+        //}
+
+
+
+
+
+
+        //static void CalcAvg(params int[] nums)
+        //{
+        //    //int sum = 0;
+        //    //int average = 0;
+        //    //for (int i = 0; i < nums.Length; i++)
+        //    //{
+        //    //    sum += nums[i];
+
+        //    //}
+        //    //average = sum / nums.Length;
+        //    //Console.WriteLine(average);
+
+        //    //if (average > 60)
+        //    //{
+        //    //    Console.WriteLine("Mezun oldunuz");
+        //    //}
+        //    //else
+        //    //{
+        //    //    Console.WriteLine("Mezun ola bilmediniz");
+        //    //}
+        //}
+    }
+}
